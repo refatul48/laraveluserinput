45cf4ae3de9c254410ef8fd45968ac9647221817
refatul48 <152413118+refatul48@users.noreply.github.com>
2024-09-19 23:22:45 +0600

    user input

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> use input form</title>
+</head>
+<body>
+
+<label for="fname">First Name</label>
+<input type="text" name="fname" placeholder="Enter you first name ">
+ <br><br>
+<label for="lname">Last Name</label>
+<input type="text" name="lname" placeholder="Enter your last name">
+
+<br><br>
+<label for="email">Email</label>
+<input type="email" name="email" placeholder="Enter your Email" >
+<br><br>
+<label for="number">Contact Number</label>
+<input type="number" name="number">
+<br>
+<br>
+
+
+<input type="submit">
+
+    
+</body>
+</html>
